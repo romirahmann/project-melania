6ff0f870a72f939e5c20d6f90837bd5a1d99a5d1
programmerpadama <itprogrammerpadama@gmail.com>
2025-05-15 10:51:42 +0700

    BACKUP

--- FE/src/context/Urlapi.js
+++ FE/src/context/Urlapi.js
@@ -6,16 +6,16 @@ let dynamicBaseAPI = "";
 let dynamicBaseURL = "";
 
 if (hostname.startsWith("192.168.9.")) {
-  dynamicBaseAPI = "http://192.168.9.192:3003/api";
-  dynamicBaseURL = "http://192.168.9.192:3003";
+  dynamicBaseAPI = "http://192.168.9.208:3003/api";
+  dynamicBaseURL = "http://192.168.9.208:3003";
 } else if (hostname.startsWith("192.168.10.")) {
-  dynamicBaseAPI = "http://192.168.10.192:3003/api";
-  dynamicBaseURL = "http://192.168.10.192:3003";
+  dynamicBaseAPI = "http://192.168.10.208:3003/api";
+  dynamicBaseURL = "http://192.168.10.208:3003";
 } else if (hostname === "localhost" || hostname === "127.0.0.1") {
   dynamicBaseAPI = "http://localhost:3003/api";
   dynamicBaseURL = "http://localhost:3003";
 } else {
-  dynamicBaseAPI = "http://192.168.10.192:3003/api";
+  dynamicBaseAPI = "http://192.168.10.208:3003/api";
 }
 
 export const ApiUrl = createContext(dynamicBaseAPI);
